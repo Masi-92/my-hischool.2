34c37ea07a5ac68b4d79285706177b4147722616
Masi-92 <masoud.izadi@92gmali.com>
2023-12-07 19:24:17 +0100

    index1

--- back-end/routers/index.js
+++ back-end/routers/index.js
@@ -1,15 +1,17 @@
 import { Router } from "express";
 import authRoutes from "./auth.route.js";
 import manageTeacherRoutes from "./managerTeacher.router.js";
-import fileRoute from "./";
+import fileRoute from "./file.route.js";
+import classRoute from "./class.route.js"
+import studentRoute from "./student.router.js"
 
 const router = Router();
 
 router.use("/auth", authRoutes);
 router.use("/auth", authRoutes);
 router.use("manageTeacher", manageTeacherRoutes);
-router.use("/class", classRouter);
-router.use("/student", studentRouter);
+router.use("/class", classRoute);
+router.use("/student", studentRoute);
 router.use("/file", fileRoute);
 
 export default router;
