4eca1603022bd337d808f9ebf8b17f2835c7b9fe
Masi-92 <masoud.izadi@92gmali.com>
2023-12-08 11:18:57 +0100

    classApi-2

--- front-end/src/api/classesApi.js
+++ front-end/src/api/classesApi.js
@@ -1,6 +1,29 @@
 import myAxios from "./api";
-
+//get 
 export const ClassesApi = {
     getClasses(){
         return myAxios.get("/class")
-}}
+},
+
+//byId
+
+getClassById(id){
+    return myAxios.get(`/class/${id}`)
+},
+
+addClass(body){
+
+    return myAxios.post("/class",body)
+},
+
+deleteClass(id){
+    return myAxios.delete(`/class/${id}`)
+},
+
+updateClass(id,body){
+    return myAxios.put(`/class/${id}`,body)
+},
+
+
+}
+
