8878abb4bd48f4dc3f43658966866b6b8bc6d2be
Masi-92 <masoud.izadi@92gmali.com>
2023-12-14 21:53:51 +0100

    manager/front

--- front-end/src/pages/login/login.jsx
+++ front-end/src/pages/login/login.jsx
@@ -1,57 +1,50 @@
 import { useState } from "react";
-
+import { useDispatch } from "react-redux";
 import { toast } from "react-toastify";
-import api from '../../api/api.js';
-import { AuthApi } from '../../api/authApi.js';
-import { loginSchema } from '../../validation/auth.validation.jsx';
+import api from "../../api/api.js";
+import { AuthApi } from "../../api/authApi.js";
+import { login } from "../../store/slice/auth.slice.js";
+import { loginSchema } from "../../validation/auth.validation.jsx";
+import style from "./login.module.scss";
 
-import { useDispatch } from 'react-redux';
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
 
-import { login } from '../../store/slice/auth.slice.js';
-
-
-
-export const Login = () => {
-const [email,setEmail] = useState("");
-const [password,setPassword]= useState("");
-const dispatch =useDispatch();
-
-function handleLogin() {
-  const { error } = loginSchema.validate({ username: email, password });
-  if (error) return toast.warn(error.message);
-  AuthApi.login(email, password)
-    .then((res) => {
-      api.defaults.headers.token = res.data.token
-      dispatch(
-        login({
-          token: res.data.token,
-          role: res.data.role,
-          fullName: res.data.fullName,
-        })
-      );
-    })
-    .catch((err) => toast.error(err));
-}
+  function handleLogin() {
+    const { error } = loginSchema.validate({ username: email, password });
+    if (error) return toast.warn(error.message);
+    AuthApi.login(email, password)
+      .then((res) => {
+        api.defaults.headers.token = res.data.token
+        dispatch(
+          login(res.data)
+        );
+      })
+      .catch((err) => toast.error(err));
+  }
 
   return (
-    <div >
-    <div>
-      <h1>Login</h1>
-      <input
-        type="text"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        placeholder="👤"
-      />
-      <input
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        placeholder="🔒"
-      />
-      <button onClick={handleLogin}>Login</button>
+    <div className={style.login}>
+      <div className={style.login_form}>
+        <h1>Login</h1>
+        <input
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="👤"
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="🔒"
+        />
+        <button onClick={handleLogin}>Login</button>
+      </div>
     </div>
-  </div>
-  )
-}
+  );
+};
+
 export default Login;
--- front-end/src/routes.jsx
+++ front-end/src/routes.jsx
@@ -1,40 +1,127 @@
-import React from 'react'
-import { useSelector } from 'react-redux';
-import { Navigate, Route, Routes } from 'react-router-dom';
-import store from './store/store';
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { Navigate, Route, Routes } from "react-router-dom";
+import Layout from "./components/layout/layout";
+import Schools from "./pages/admin/schools/schools";
+import Landing from "./pages/landing/landing";
+import Loader from "./pages/loader/loader";
+import Login from "./pages/login/login";
+import ClassForm from "./pages/manager/classForm/classForm";
+import Classes from "./pages/manager/classes/classes";
+import Events from "./pages/manager/events/events";
+import NewsFeed from "./pages/manager/newsFeed/newsFeed";
+import StudentForm from "./pages/manager/studentForm/studentForm";
+import Students from "./pages/manager/students/students";
+import TeacherStudents from "./pages/teacher/students/students";
+import TeacherForm from "./pages/manager/teacherForm/teacherForm";
+import Teachers from "./pages/manager/teachers/teachers";
+import FeedParent from "./pages/parent/feed/feed";
+import Register from "./pages/register/register";
+import Feed from "./pages/teacher/feed/feed";
 import { Roles } from "./store/slice/auth.slice";
-import  Login  from './pages/login/login';
+import Forum from "./pages/forum/forum";
 
 const AppRoutes = () => {
+  const { isAuthenticated, role } = useSelector((store) => store.auth);
 
-    const {isAuthenticated,role}=useSelector((store)=> store.auth)
+  function hasRole(_role) {
+    return isAuthenticated && role === _role;
+  }
 
-    function getPanelAddress() {
-        switch (role) {
-          case Roles.MANAGER:
-            return "/manager";
-          case Roles.TEACHER:
-            return "/teacher";
-          case Roles.PARENT:
-            return "/parent";
-          case Roles.SUPER_ADMIN:
-            return "/admin";
-          default:
-            return "/";
-        }
-      }
-
+  function getPanelAddress() {
+    switch (role) {
+      case Roles.MANAGER:
+        return "/manager";
+      case Roles.TEACHER:
+        return "/teacher";
+      case Roles.PARENT:
+        return "/parent";
+      case Roles.SUPER_ADMIN:
+        return "/admin";
+      default:
+        return "/";
+    }
+  }
+  const [showLoader, setShowLoader] = useState(false);
 
   return (
-   <Routes>
-  <Route
-        path="/login"
-        element={isAuthenticated ? <Navigate to={getPanelAddress()} /> : <Login />}
-      />
+    <>
+      {showLoader ? (
+        <Loader setShowLoader={setShowLoader} />
+      ) : (
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/preRegister" element={<Register />} />
+          <Route
+            path="/login"
+            element={
+              isAuthenticated ? <Navigate to={getPanelAddress()} /> : <Login />
+            }
+          />
 
-
-   </Routes>
-  )
-}
+          <Route
+            path="/manager/*"
+            element={
+              hasRole(Roles.MANAGER) ? <Layout /> : <Navigate to="/login" />
+            }
+          >
+            <Route path="teachers" element={<Teachers />}></Route>
+            <Route path="teachers/add" element={<TeacherForm />}></Route>
+            <Route
+              path="teachers/edit/:teacherId"
+              element={<TeacherForm />}
+            ></Route>
+            <Route path="classes" element={<Classes />}></Route>
+            <Route path="classes/add" element={<ClassForm />}></Route>
+            <Route path="classes/edit/:classId" element={<ClassForm />}></Route>
+            <Route path="students" element={<Students />}></Route>
+            <Route path="students/add" element={<StudentForm />}></Route>
+            <Route
+              path="students/edit/:studentId"
+              element={<StudentForm />}
+            ></Route>
+            <Route path="events" element={<Events />}></Route>
+            <Route path="feed" element={<NewsFeed />}></Route>
+            <Route path="forum" element={<Forum />}></Route>
+            <Route
+              path=""
+              element={<Navigate to="/manager/teachers" />}
+            ></Route>
+          </Route>
+          <Route
+            path="/teacher/*"
+            element={
+              hasRole(Roles.TEACHER) ? <Layout /> : <Navigate to="/login" />
+            }
+          >
+            <Route path="feed" element={<NewsFeed />}></Route>
+            <Route path="events" element={<Events />}></Route>
+            <Route path="students" element={<TeacherStudents />}></Route>
+            <Route path="" element={<Navigate to="/teacher/feed" />}></Route>
+          </Route>
+          <Route
+            path="/parent/*"
+            element={
+              hasRole(Roles.PARENT) ? <Layout /> : <Navigate to="/login" />
+            }
+          >
+            <Route path="feed" element={<FeedParent />}></Route>
+            <Route path="preRegister" element={<p>register parent</p>}></Route>
+            <Route path="" element={<Navigate to="/parent/feed" />}></Route>
+          </Route>
+          <Route
+            path="/admin/*"
+            element={
+              hasRole(Roles.SUPER_ADMIN) ? <Layout /> : <Navigate to="/login" />
+            }
+          >
+            <Route path="schools" element={<Schools />}></Route>
+            <Route path="" element={<Navigate to="/admin/schools" />}></Route>
+          </Route>
+        </Routes>
+      )}
+    </>
+  );
+};
 
 export default AppRoutes;
