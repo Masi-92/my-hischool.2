f4519e1ae1382d5c619e5a39552f74dfe0f7908c
Masi-92 <masoud.izadi@92gmali.com>
2024-02-06 22:12:34 +0100

    main

--- front-end/src/components/header/header.jsx
+++ front-end/src/components/header/header.jsx
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import { Logout } from "@mui/icons-material";
-=======
-import { DarkMode, Language, LightMode, Logout } from "@mui/icons-material";
->>>>>>> masi
 import AccountCircle from "@mui/icons-material/AccountCircle";
 import MenuIcon from "@mui/icons-material/Menu";
 import AppBar from "@mui/material/AppBar";
@@ -13,20 +9,6 @@ import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import * as React from "react";
 import { useDispatch, useSelector } from "react-redux";
-<<<<<<< HEAD
-import { logout } from "../../store/slice/auth.slice";
-import style from "./header.module.scss";
-import { Logo } from "../logo/logo";
-import  animation  from "./logo.json"
-import Lottie from "lottie-react";
-import { Avatar } from "@mui/material";
-
-export default function Header() {
-  const { image, role, fullName } = useSelector(
-    (store) => store.auth
-  );
-  const [anchorEl, setAnchorEl] = React.useState(null);
-=======
 import { Roles, logout } from "../../store/slice/auth.slice";
 import style from "./header.module.scss";
 import { Logo } from "../logo/logo";
@@ -49,13 +31,11 @@ export default function Header({ handleToggleMenu }) {
   const { t: tRole } = useTranslation("translation", {
     keyPrefix: "role",
   });
->>>>>>> masi
 
   const dispatch = useDispatch();
 
   const handleLogout = () => {
     dispatch(logout());
-<<<<<<< HEAD
     handleClose();
   };
 
@@ -71,76 +51,11 @@ export default function Header({ handleToggleMenu }) {
     <AppBar className={style.header} position="static">
       <Toolbar>
         {/* <IconButton
-=======
-    handleCloseProfile();
-  };
-
-  const handleEditProfile = () => {
-    navigate(`/${role}/editProfile`);
-    handleCloseProfile();
-  };
-
-  const handleMenuProfile = (event) => {
-    setAnchorElProfile(event.currentTarget);
-  };
-
-  const handleMenuLang = (event) => {
-    setAnchorElLang(event.currentTarget);
-  };
-
-  const handleCloseProfile = () => {
-    setAnchorElProfile(null);
-  };
-  const handleCloseLang = () => {
-    setAnchorElLang(null);
-  };
-
-  const headerColor = useMemo(() => {
-    if (theme === "dark") return "#121212";
-    switch (role) {
-      case Roles.MANAGER:
-        return "Pink";
-      case Roles.PARENT:
-        return "Orange";
-      case Roles.TEACHER:
-        return "Blue";
-    }
-  }, [role,theme]);
-
-  const handleChangeLang = (lang) => {
-    i18n.changeLanguage(lang);
-    handleCloseLang();
-    localStorage.setItem("lang", lang);
-    switch (lang) {
-      case "en":
-      case "de":
-        dispatch(changeLayout("ltr"));
-        break;
-      case "fa":
-        dispatch(changeLayout("rtl"));
-        break;
-    }
-  };
-
-  const translateRole = (role) => {
-    return tRole(role);
-  };
-
-  return (
-    <AppBar
-      className={style.header}
-      style={{ backgroundColor: headerColor }}
-      position="static"
-    >
-      <Toolbar>
-        <IconButton
->>>>>>> masi
           size="large"
           edge="start"
           color="inherit"
           aria-label="menu"
           sx={{ mr: 2 }}
-<<<<<<< HEAD
         >
           <MenuIcon />
         </IconButton> */}
@@ -156,57 +71,20 @@ export default function Header({ handleToggleMenu }) {
         {/* <AccountCircle/> */}
         <Typography>
           {fullName} - {role}
-=======
-          onClick={handleToggleMenu}
-        >
-          <MenuIcon />
-        </IconButton>
-        <div variant="h6" component="div" style={{ flexGrow: 1 }}>
-          <Lottie
-            fill="#40C0E7"
-            animationData={animation}
-            style={{
-              position: "relative",
-              width: "60px",
-              // height: "5vw",
-              // marginTop: "-15px",
-              // marginLeft: "-20px",
-            }}
-          />
-        </div>
-        {/* <AccountCircle/> */}
-        <Typography className={style.username}>
-          {fullName} - {translateRole(role)}
->>>>>>> masi
         </Typography>
         <IconButton
           size="large"
           aria-label="account of current user"
           aria-controls="menu-appbar"
           aria-haspopup="true"
-<<<<<<< HEAD
           onClick={handleMenu}
-=======
-          onClick={handleMenuProfile}
->>>>>>> masi
           color="inherit"
         >
           <Avatar src={image}></Avatar>
         </IconButton>
-<<<<<<< HEAD
         <Menu
           id="menu-appbar"
           anchorEl={anchorEl}
-=======
-        <IconButton size="large" onClick={handleMenuLang} color="inherit">
-          <Language></Language>
-        </IconButton>
-        <IconButton onClick={() => dispatch(toggleTheme())}>
-          {theme === "light" ? <DarkMode></DarkMode> : <LightMode></LightMode>}
-        </IconButton>
-        <Menu
-          anchorEl={anchorElProfile}
->>>>>>> masi
           anchorOrigin={{
             vertical: "bottom",
             horizontal: "right",
@@ -216,38 +94,11 @@ export default function Header({ handleToggleMenu }) {
             vertical: "top",
             horizontal: "right",
           }}
-<<<<<<< HEAD
           open={Boolean(anchorEl)}
           onClose={handleClose}
         >
           {/* <MenuItem onClick={handleClose}>Profile</MenuItem> */}
           <MenuItem onClick={handleLogout}>Logout</MenuItem>
-=======
-          open={Boolean(anchorElProfile)}
-          onClose={handleCloseProfile}
-        >
-          <MenuItem onClick={handleEditProfile}>{t("profile")}</MenuItem>
-          <MenuItem onClick={handleLogout}>{t("logout")}</MenuItem>
-        </Menu>
-
-        <Menu
-          anchorEl={anchorElLang}
-          anchorOrigin={{
-            vertical: "bottom",
-            horizontal: "right",
-          }}
-          keepMounted
-          transformOrigin={{
-            vertical: "top",
-            horizontal: "right",
-          }}
-          open={Boolean(anchorElLang)}
-          onClose={handleCloseLang}
-        >
-          <MenuItem onClick={() => handleChangeLang("en")}>English</MenuItem>
-          <MenuItem onClick={() => handleChangeLang("de")}>Deutsch</MenuItem>
-          <MenuItem onClick={() => handleChangeLang("fa")}>فارسی</MenuItem>
->>>>>>> masi
         </Menu>
       </Toolbar>
     </AppBar>
--- front-end/src/components/sidebar/sidebar.jsx
+++ front-end/src/components/sidebar/sidebar.jsx
@@ -10,11 +10,6 @@ import {
   Menu,
   School,
   Sick,
-<<<<<<< HEAD
-=======
-  Draw,
-  SchoolTwoTone,
->>>>>>> masi
 } from "@mui/icons-material";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import Divider from "@mui/material/Divider";
@@ -26,25 +21,11 @@ import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import { styled } from "@mui/material/styles";
-<<<<<<< HEAD
 import React, { useMemo } from "react";
-=======
-import React, { useEffect, useMemo, useState } from "react";
->>>>>>> masi
 import { useSelector } from "react-redux";
 import { Link, NavLink } from "react-router-dom";
 import { Roles } from "../../store/slice/auth.slice";
 import style from "./sidebar.module.scss";
-<<<<<<< HEAD
-=======
-import { Card, useMediaQuery } from "@mui/material";
-import { StudentsApi } from "../../api/studentApi";
-import { toast } from "react-toastify";
-import { AuthApi } from "../../api/authApi";
-import HistoryEduIcon from "@mui/icons-material/HistoryEdu";
-import { SchoolsApi } from "../../api/schoolApi";
-import { useTranslation } from "react-i18next";
->>>>>>> masi
 
 const drawerWidth = 240;
 
@@ -95,7 +76,6 @@ const DrawerHeader = styled("div")(({ theme }) => ({
   ...theme.mixins.toolbar,
 }));
 
-<<<<<<< HEAD
 const superAdminMenus = [
   {
     label: "Schools",
@@ -207,150 +187,6 @@ const Sidebar = () => {
   const theme = useTheme();
   const role = useSelector((store) => store.auth.role);
   const [open, setOpen] = React.useState(true);
-=======
-
-const Sidebar = ({ open, setOpen }) => {
-  const theme = useTheme();
-  const role = useSelector((store) => store.auth.role);
-  const [myTeacher, setMyTeacher] = useState();
-  const [isFreeTeacher, setIsFreeTeacher] = useState(false);
-  const [school, setSchool] = useState();
-  const desktopSize = useMediaQuery("(min-width:900px)");
-  const { t } = useTranslation("translation", { keyPrefix: "sidebar" });
-
-  useEffect(() => {
-    if (role === Roles.PARENT) {
-      StudentsApi.getMyTeacher()
-        .then((res) => {
-          setMyTeacher(res.data);
-        })
-        .catch((err) => toast.error(err));
-    } else if (role === Roles.TEACHER) {
-      AuthApi.getProfile().then((res) => {
-        if (res.data.freeTeacher) {
-          setIsFreeTeacher(true);
-        } else setIsFreeTeacher(false);
-      });
-    }
-    if (role !== Roles.SUPER_ADMIN)
-      SchoolsApi.getMySchool()
-        .then((res) => {
-          setSchool(res.data);
-        })
-        .catch((err) => toast.error(err));
-  }, []);
-
-  const parentMenus = useMemo(
-    () => [
-      {
-        label: t("newsFeed"),
-        link: "/parent/feed",
-        icon: <Feed />,
-      },
-      {
-        label: t("forum"),
-        link: "/parent/forum",
-        icon: <Forum />,
-      },
-      {
-        label: t("calendar"),
-        link: "/parent/calendar",
-        icon: <CalendarMonth />,
-      },
-      {
-        label: t("teacherPv"),
-        link: `/pv/${myTeacher?._id}`,
-        icon: <Chat />,
-      },
-      {
-        label: t("sickRest"),
-        link: "/parent/sickRest",
-        icon: <Sick />,
-      },
-      {
-        label: t("events"),
-        link: "/parent/events",
-        icon: <Event />,
-      },
-    ],
-    [myTeacher, t]
-  );
-
-  const teacherMenus = [
-    {
-      label: t("newsFeed"),
-      link: "/teacher/feed",
-      icon: <Feed />,
-    },
-    {
-      label: t("forum"),
-      link: "/teacher/forum",
-      icon: <Forum />,
-    },
-    {
-      label: t("students"),
-      link: "/teacher/students",
-      icon: <Forum />,
-    },
-    {
-      label: t("events"),
-      link: "/teacher/events",
-      icon: <Event />,
-    },
-    {
-      label: t("sickRest"),
-      link: "/teacher/sick",
-      icon: <Sick />,
-    },
-  ];
-
-  const managerMenus = [
-    {
-      label: t("teachers"),
-      link: "/manager/teachers",
-      icon: <HistoryEduIcon />,
-    },
-    {
-      label: t("classList"),
-      link: "/manager/classes",
-      icon: <Class />,
-    },
-    {
-      label: t("students"),
-      link: "/manager/students",
-      icon: <People />,
-    },
-    {
-      label: t("events"),
-      link: "/manager/events",
-      icon: <Event />,
-    },
-    {
-      label: t("newsFeed"),
-      link: "/manager/feed",
-      icon: <Feed />,
-    },
-    {
-      label: t("forum"),
-      link: "/manager/forum",
-      icon: <Forum />,
-    },
-    {
-      label: t("sickRest"),
-      link: "/manager/sickRest",
-      icon: <Sick />,
-    },
-  ];
-
-  const superAdminMenus = [
-    {
-      label: t("schools"),
-      link: "/admin/schools",
-      icon: <School />,
-    },
-  ];
-  
->>>>>>> masi
 
   const menus = useMemo(() => {
     switch (role) {
@@ -359,26 +195,16 @@ const Sidebar = ({ open, setOpen }) => {
       case Roles.MANAGER:
         return managerMenus;
       case Roles.TEACHER:
-<<<<<<< HEAD
         return teacherMenus;
       case Roles.PARENT:
         return parentMenus;
     }
   }, [role]);
-=======
-        if (!isFreeTeacher) return teacherMenus;
-        else return teacherMenus.slice(0, teacherMenus.length - 1);
-      case Roles.PARENT:
-        return parentMenus;
-    }
-  }, [role, parentMenus, isFreeTeacher, t]);
->>>>>>> masi
 
   const handleDrawerToggle = () => {
     setOpen((open) => !open);
   };
 
-<<<<<<< HEAD
   return (
     <Drawer style={{ zIndex: 1 }} variant="permanent" open={open}>
       <DrawerHeader>
@@ -424,87 +250,6 @@ const Sidebar = ({ open, setOpen }) => {
         ))}
       </List>
     </Drawer>
-=======
-  const handleMenuClick = () => {
-    if (!desktopSize) setOpen(false);
-  };
-
-  const renderSidebarContent = () => {
-    return (
-      <>
-        <DrawerHeader>
-          <IconButton onClick={handleDrawerToggle}>
-            <ChevronRightIcon />
-          </IconButton>
-        </DrawerHeader>
-        <Divider />
-        <List>
-          {menus.map((menu, index) => (
-            <NavLink
-              className={style.link}
-              style={({ isActive }) => ({
-                color: isActive ? "#4db5ff" : "gray",
-              })}
-              key={menu.link}
-              to={menu.link}
-              onClick={handleMenuClick}
-            >
-              <ListItem key={menu} disablePadding sx={{ display: "block" }}>
-                <ListItemButton
-                  sx={{
-                    minHeight: 48,
-                    justifyContent: open ? "initial" : "center",
-                    px: 2.5,
-                  }}
-                >
-                  <ListItemIcon
-                    sx={{
-                      minWidth: 0,
-                      mr: open ? 3 : "auto",
-                      justifyContent: "center",
-                    }}
-                  >
-                    {menu.icon}
-                  </ListItemIcon>
-                  <ListItemText
-                    primary={menu.label}
-                    sx={{ opacity: open ? 1 : 0 }}
-                  />
-                </ListItemButton>
-              </ListItem>
-            </NavLink>
-          ))}
-        </List>
-        {school && (
-          <div className={style.school}>
-            <span>{school?.name}</span>
-            <a href={`tel:${school?.tel}`}>{school?.tel}</a>
-          </div>
-        )}
-      </>
-    );
-  };
-
-  if (desktopSize)
-    return (
-      <Drawer variant="permanent" style={{ zIndex: 1 }} open={open}>
-        {renderSidebarContent()}
-      </Drawer>
-    );
-
-  return (
-    <div>
-      <MuiDrawer
-        style={{ zIndex: 1 }}
-        anchor="left"
-        variant={"temporary"}
-        onClose={() => setOpen(false)}
-        open={open}
-      >
-        {renderSidebarContent()}
-      </MuiDrawer>
-    </div>
->>>>>>> masi
   );
 };
 
