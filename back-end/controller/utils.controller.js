d6dde255532a3ddf99a12ff15cd8798c5468af48
Masi-92 <masoud.izadi@92gmali.com>
2024-02-09 02:36:24 +0100

    commt

--- back-end/controller/utils.controller.js
+++ back-end/controller/utils.controller.js
@@ -13,18 +13,18 @@ export const getSchoolIdOfTeacherById = async (teacherId) => {
   return teacher.school;
 };
 
-export const generateUniqueFileName = (fileName, encoded) => {
-  fileName.charAt(0)
-  const textToChars = (text) => text.split("").map((c) => c.charCodeAt(0));
-  const applySaltToChar = (code) =>
-    textToChars("secret").reduce((a, b) => a ^ b, code);
-  return encoded
-    .match(/.{1,2}/g)
-    .map((hex) => parseInt(hex, 16))
-    .map(applySaltToChar)
-    .map((charCode) => String.fromCharCode(charCode))
-    .join("");
-};
+// export const generateUniqueFileName = (fileName, encoded) => {
+//   fileName.charAt(0)
+//   const textToChars = (text) => text.split("").map((c) => c.charCodeAt(0));
+//   const applySaltToChar = (code) =>
+//     textToChars("secret").reduce((a, b) => a ^ b, code);
+//   return encoded
+//     .match(/.{1,2}/g)
+//     .map((hex) => parseInt(hex, 16))
+//     .map(applySaltToChar)
+//     .map((charCode) => String.fromCharCode(charCode))
+//     .join("");
+// };
 
 
 
